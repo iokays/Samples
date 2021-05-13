bd64549328209d1f4d27d7a2fb34ce72b612b0f1
彭元彬 <pengyuanbing@chngalaxy.com>
2021-05-13 19:41:45 +0800

    添加飞行,呱呱叫行为.

---
+++ design_pattern/src/main/java/com/iokays/design/pattern/delegate/FlyBehavior.java
@@ -0,0 +1,8 @@
+package com.iokays.design.pattern.delegate;
+
+/**
+ * 飞行行为接口
+ */
+public interface FlyBehavior {
+    void fly();
+}
---
+++ design_pattern/src/main/java/com/iokays/design/pattern/delegate/QuackBehavior.java
@@ -0,0 +1,8 @@
+package com.iokays.design.pattern.delegate;
+
+/**
+ * 呱呱叫行为接口
+ */
+public interface QuackBehavior {
+    void quack();
+}
