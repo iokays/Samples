e30b3df8764ec913aeb7ae6cedbfb2390150201c
彭元彬 <pengyuanbing@chngalaxy.com>
2021-05-21 18:31:30 +0800

    update.

--- spring_web/src/main/java/com/iokays/web/boot/SpringMvcSample.java
+++ spring_web/src/main/java/com/iokays/web/boot/SpringMvcSample.java
@@ -1,16 +1,23 @@
-package com.iokays.web;
+package com.iokays.web.boot;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @SpringBootApplication
 public class SpringMvcSample {
 
+    @ApiOperation(value = "/", notes = "print hello")
     @GetMapping(value = "/")
-    public String hello() {
+    public String hello(@RequestParam(value = "value", required = false) String value,
+                        HttpServletRequest request, HttpServletResponse response) {
         return "hello";
     }
 
---
+++ spring_web/src/main/java/com/iokays/web/boot/WebConfig.java
@@ -0,0 +1,49 @@
+package com.iokays.web.boot;
+
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                if (handler instanceof HandlerMethod) {
+                    HandlerMethod handlerMethod = (HandlerMethod) handler;
+                    final ApiOperation apiOperation = handlerMethod.getMethodAnnotation(ApiOperation.class);
+
+                    logger.info("handlerMethod: {}, apiOperation.notes: {}", handler, apiOperation.notes());
+
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+    }
+
+}
