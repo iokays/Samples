cff13d4df256e86e126ff2915008327bc0d2ea15
彭元彬 <pengyuanbing@chngalaxy.com>
2022-01-14 18:01:47 +0800

    添加备忘录模式.

---
+++ effective_java/src/main/java/designpatterns/memento/Memento.java
@@ -0,0 +1,15 @@
+package designpatterns.memento;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+}
