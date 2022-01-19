5d7be4b5b82c247977fa0e0eab8f9f8dee1fbbbf
彭元彬 <pengyuanbing@chngalaxy.com>
2022-01-19 15:47:59 +0800

    添加工厂方法模式.

--- effective_java/src/main/java/com/iokays/designpatterns/simplefactory/Pizza.java
+++ effective_java/src/main/java/com/iokays/designpatterns/simplefactory/Pizza.java
@@ -1,20 +1,37 @@
 package com.iokays.designpatterns.simplefactory;
 
+import java.util.ArrayList;
+
 public abstract class Pizza {
 
-    void prepare() {
-        System.out.println("prepare");
+    protected String name;
+    protected String dough;
+    protected String sauce;
+    protected ArrayList toppings = new ArrayList();
+
+    public void prepare() {
+        System.out.printf("Preparing %s\n", name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings: ");
+        for (int i = 0; i < toppings.size(); i++) {
+            System.out.printf("   %s\n", toppings.get(i));
+        }
     }
 
-    void bake() {
-        System.out.println("bake");
+    public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
     }
 
-    void cut() {
-        System.out.println("cut");
+    public void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
     }
 
-    void box() {
-        System.out.println("box");
+    public void box() {
+        System.out.println("Place pizza in official PizzaStore box");
+    }
+
+    public String getName() {
+        return name;
     }
 }
