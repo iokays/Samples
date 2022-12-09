cf4e1b91f160760e27b0b3c4ec2eb799c7d849a7
彭元彬 <pengyuanbing@gmail.com>
2022-12-09 10:48:15 +0800

    update
    update

--- work/src/test/java/com/iokays/work/domain/ApplicationServiceTest.java
+++ work/src/test/java/com/iokays/work/domain/ApplicationServiceTest.java
@@ -4,18 +4,14 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @SpringBootTest(classes = Application.class)
 public class ApplicationServiceTest {
 
     @Resource
     ElasticsearchOperations operations;
-    @Resource
-    ConferenceRepository repository;
 
     @Resource
     private ApplicationService applicationService;
@@ -23,17 +19,6 @@ public class ApplicationServiceTest {
     @Test
     public void transfer() throws IOException {
         applicationService.transfer();
-    }
-
-    @Test
-    public void insertDataSample() {
-        operations.indexOps(Conference.class).refresh();
-        // Save data sample
-        var c = new Conference();
-        c.setDate("2014-11-06");
-        var documents = Arrays.asList(c);
-        repository.saveAll(documents);
-        operations.indexOps(Conference.class).refresh();
     }
 
     @Test
