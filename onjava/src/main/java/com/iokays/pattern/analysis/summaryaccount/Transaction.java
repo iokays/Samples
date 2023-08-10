9e797809cd79dd309a70e92376543db26d9715b0
彭元兵 <pengyb06@vanke.com>
2023-08-10 20:19:23 +0800

    update

--- onjava/src/main/java/com/iokays/pattern/analysis/summaryaccount/Transaction.java
+++ onjava/src/main/java/com/iokays/pattern/analysis/summaryaccount/Transaction.java
@@ -1,24 +1,24 @@
-package com.iokays.pattern.analysis.summaryaccount;
-
-import Account;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashSet;
-
-public class Transaction {
-
-    private Collection<Entry> entries = new HashSet<>();
-
-    public Transaction(BigDecimal amount, Account from, Account to, LocalDateTime whenCharged, LocalDateTime whenBooked) {
-        Entry fromEntry = new Entry(amount.negate(), whenCharged, whenBooked);
-        from.addEntry(fromEntry);
-        this.entries.add(fromEntry);
-
-        Entry toEntry = new Entry(amount, whenCharged, whenBooked);
-        to.addEntry(toEntry);
-        this.entries.add(toEntry);
-    }
-
-}
+//package com.iokays.pattern.analysis.summaryaccount;
+//
+//import Account;
+//
+//import java.math.BigDecimal;
+//import java.time.LocalDateTime;
+//import java.util.Collection;
+//import java.util.HashSet;
+//
+//public class Transaction {
+//
+//    private Collection<Entry> entries = new HashSet<>();
+//
+//    public Transaction(BigDecimal amount, Account from, Account to, LocalDateTime whenCharged, LocalDateTime whenBooked) {
+//        Entry fromEntry = new Entry(amount.negate(), whenCharged, whenBooked);
+//        from.addEntry(fromEntry);
+//        this.entries.add(fromEntry);
+//
+//        Entry toEntry = new Entry(amount, whenCharged, whenBooked);
+//        to.addEntry(toEntry);
+//        this.entries.add(toEntry);
+//    }
+//
+//}
