9bcad6f66dfe5b5dbba9c44898048b38862bdabf
彭元兵 <pengyb06@vanke.com>
2023-08-30 15:08:47 +0800

    添加 Organization Hierarchy

---
+++ A1.analysis_pattern/src/main/java/com/iokays/analysispattern/accountability/Company.java
@@ -0,0 +1,9 @@
+package com.iokays.analysispattern.accountability;
+
+public class Company extends Organization {
+
+    public Company(String name, Organization parent) {
+        super(name, parent);
+    }
+
+}
