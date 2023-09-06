b1edacbe82b8167c9cf6fb0dcc3283348c7c56ae
彭元兵 <pengyb06@vanke.com>
2023-09-06 18:15:57 +0800

    添加区块链的用例。

---
+++ A0.on_java/src/main/java/com/iokays/onjava/blockchain/Tester.java
@@ -0,0 +1,35 @@
+package com.iokays.onjava.blockchain;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Tester implements Serializable {
+
+    @Test
+    public void testBlock() {
+
+        final Block firstBlock = new Block("0", "first block");
+        final var difficulty = 6;
+
+        firstBlock.mineBlock(difficulty);
+        System.out.println("first block hash : " + firstBlock.getHash());
+
+        final Block secondBlock = new Block(firstBlock.getHash(), "second block");
+        secondBlock.mineBlock(difficulty);
+        System.out.println("second block hash : " + secondBlock.getHash());
+
+        final Block thirdBlock = new Block(secondBlock.getHash(), "third block");
+        thirdBlock.mineBlock(difficulty);
+        System.out.println("third block hash : " + thirdBlock.getHash());
+
+        final var list = Stream.of(firstBlock, secondBlock, thirdBlock).toList();
+        Block.isChainValid(list);
+
+        list.forEach(System.out::println);
+    }
+
+}
